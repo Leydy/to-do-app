e3231f834076b10eb090aeebc48b538e34326b19
Leydy <leydymayumy@gmail.com>
2018-08-24 11:50:26 -0500

    trabajando con listas

---
+++ Body.js
@@ -0,0 +1,29 @@
+// import liraries
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+
+// create a component
+class Body extends Component {
+  render() {
+    return (
+            <View style={styles.container}>
+                <Text>Body</Text>
+                <FlatList
+                  data={this.props.tareas}
+                  renderItem={({ item }) => <Text>{item.texto}</Text>}
+                />
+            </View>
+    );
+  }
+}
+
+// define your styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 2,
+    backgroundColor: '#F781F3',
+  },
+});
+
+// make this component available to the app
+export default Body;
