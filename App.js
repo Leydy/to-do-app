47ebb89ba17a78dc681afe728bb2d6b6e6e950f1
Leydy <leydymayumy@gmail.com>
2018-08-29 12:03:55 -0500

    AsyncStorage y ciclos de vida de los componentes de react

--- App.js
+++ App.js
@@ -13,29 +13,36 @@ export default class App extends React.Component {
     };
   }
 
+  componentDidMount() { // se ejecuta despues del render,almenos un elemento de render
+    this.recuperarEnTelefono();
+  }
+
   establecerTexto = (value) => {
     console.log(value);
     this.setState({ texto: value });
   }
 
   agregarTarea = () => {
+    const nuevasTareas = [...this.state.tareas, { texto: this.state.texto, key: Date.now() }];
+    this.guardarEnTelefono(nuevasTareas);
     this.setState({
-      tareas: [...this.state.tareas, { texto: this.state.texto, key: Date.now() }],
+      tareas: nuevasTareas,
       texto: '',
     });
   }
 
+
   eliminarTarea = (id) => {
-    const nuevasTareas = this.state.tareas.filter((tarea) => {
-      return tarea.key !== id; 
-    });
+    const nuevasTareas = this.state.tareas.filter(tarea => tarea.key !== id);
+    this.guardarEnTelefono(nuevasTareas);
     this.setState({
       tareas: nuevasTareas,
     });
   }
 
-  guardarEnTelefono = async () => {
-    await AsyncStorage.setItem('@CursoUdemy:arrayUno', JSON.stringify([{ key: 1, texto: 'uno' }, { key: 2, texto: 'dos' }]))
+
+  guardarEnTelefono = async (tareas) => {
+    await AsyncStorage.setItem('@CursoUdemy:tareas', JSON.stringify(tareas))
       .then((valor) => {
         console.log(valor);
       })
@@ -43,39 +50,39 @@ export default class App extends React.Component {
         console.log(error);
       });
   }
+
     
-   recuperarEnTelefono = async () => {
-     await AsyncStorage.getItem('@CursoUdemy:arrayUno')
-       .then((valor) => {
-         console.log(valor);
-         console.log(JSON.parse(valor));
-       })
-       .catch((error) => {
-         console.log(error);
-       });
-   }
+  recuperarEnTelefono = async () => {
+    await AsyncStorage.getItem('@CursoUdemy:tareas')
+      .then((valor) => {
+        console.log(valor);
+        console.log(JSON.parse(valor));
+        if (valor !== null) {
+          const nuevasTareas = JSON.parse(valor);
+          this.setState({
+            tareas: nuevasTareas,
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
   
-   render() {
-     return (
+  render() {
+    return (
       <View style={styles.container}>
         <Header
           texto={this.state.texto}
           cambiarTexto={this.establecerTexto}
           agregar={this.agregarTarea}
         />  
-        <Button
-          title="guardando"
-          onPress={() => { this.guardarEnTelefono(); }}
-        />
-        <Button
-          title="recuperando"
-          onPress={() => { this.recuperarEnTelefono(); }}
-        />
+        
         <Body tareas={this.state.tareas} eliminar={this.eliminarTarea} />
       </View>
-     );
-   }
+    );
+  }
 }
 
 const styles = StyleSheet.create({
